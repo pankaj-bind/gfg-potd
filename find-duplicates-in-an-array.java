a90b3ce2d4d95d1dc5bd7437b06d7dd6add1edc0
Pankaj Bind <pankajbind30@gmail.com>
2023-09-24 00:04:06 +0530

    24th sept updated

--- find-duplicates-in-an-array.java
+++ find-duplicates-in-an-array.java
@@ -1,3 +1,29 @@
-public class find-duplicates-in-an-array {
-    
+class Solution {
+    public static ArrayList<Integer> duplicates(int arr[], int n) {
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        // Traverse the input array and use the auxiliary array to count occurrences
+        int[] count = new int[n];
+        for (int i = 0; i < n; i++) {
+            int num = arr[i];
+            count[num]++;
+        }
+        
+        // Check which elements occur more than once and add them to the result
+        for (int i = 0; i < n; i++) {
+            if (count[i] > 1) {
+                result.add(i);
+            }
+        }
+        
+        // If no duplicates are found, return a list containing -1
+        if (result.isEmpty()) {
+            result.add(-1);
+        }
+        
+        return result;
+    }
 }
+
+
+
