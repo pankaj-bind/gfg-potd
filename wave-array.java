0368774e93b9401da568a095da9697ec05956b33
Pankaj Bind <pankajbind30@gmail.com>
2023-09-28 09:41:22 +0530

    28th sept added

---
+++ wave-array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static void convertToWave(int n, int[] a) {
+        for (int i = 0; i < n - 1; i += 2) {
+            int temp = a[i];
+            a[i] = a[i + 1];
+            a[i + 1] = temp;
+        }
+    }
+}
