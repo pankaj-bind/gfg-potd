40842a643d54fd14217e17e11ee8311e7b19fbb4
Pankaj Kumar Bind <pankajbind30@gmail.com>
2023-10-08 01:13:40 +0530

    8th oct updated

--- insert-in-a-sorted-list.java
+++ insert-in-a-sorted-list.java
@@ -1,3 +1,18 @@
-public class insert-in-a-sorted-list {
-    
+public class Solution {
+    Node sortedInsert(Node head1, int key) {
+        Node newNode = new Node(key);
+        if (head1 == null || key < head1.data) {
+            newNode.next = head1;
+            return newNode;
+        }
+
+        Node current = head1;
+        while (current.next != null && current.next.data < key) {
+            current = current.next;
+        }
+
+        newNode.next = current.next;
+        current.next = newNode;
+        return head1;
+    }
 }
