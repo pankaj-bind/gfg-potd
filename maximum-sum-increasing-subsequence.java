04299d15a0e4c329e1041b799f9795232099f4dc
Pankaj Bind <pankajbind30@gmail.com>
2023-10-23 09:25:32 +0530

    23rd oct updated

--- maximum-sum-increasing-subsequence.java
+++ maximum-sum-increasing-subsequence.java
@@ -1,3 +1,22 @@
-public class maximum-sum-increasing-subsequence {
-    
+class Solution
+{
+    public int maxSumIS(int arr[], int n)  
+    {  
+        int dp[] = new int[n];
+        for(int i = 0; i < n; i++) {
+            dp[i] = arr[i];
+            for(int j = 0; j < i; j++) {
+                if(arr[i] > arr[j] && dp[i] < dp[j] + arr[i]) {
+                    dp[i] = dp[j] + arr[i];
+                }
+            }
+        }
+        
+        int maxSum = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++) {
+            maxSum = Math.max(maxSum, dp[i]);
+        }
+        
+        return maxSum;
+    }  
 }
