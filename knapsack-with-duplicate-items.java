fd5d2b676b47fc9b85ad85cd175e1082341f5ab5
Pankaj Bind <pankajbind30@gmail.com>
2023-10-25 05:08:25 +0530

    25th oct updated

--- knapsack-with-duplicate-items.java
+++ knapsack-with-duplicate-items.java
@@ -1,3 +1,17 @@
-public class knapsack-with-duplicate-items {
-    
+class Solution{
+    static int knapSack(int N, int W, int val[], int wt[])
+    {
+        int[] dp = new int[W + 1];
+        dp[0] = 0; // Initialize dp array with zeros
+        
+        for (int i = 1; i <= W; i++) {
+            for (int j = 0; j < N; j++) {
+                if (wt[j] <= i) {
+                    dp[i] = Math.max(dp[i], dp[i - wt[j]] + val[j]);
+                }
+            }
+        }
+        
+        return dp[W];
+    }
 }
