34c557b094c43b28c4ff2628346c7f8edca60485
Pankaj Bind <pankajbind30@gmail.com>
2023-10-31 08:23:58 +0530

    31st oct updated

--- move-all-zeroes-to-end-of-array.java
+++ move-all-zeroes-to-end-of-array.java
@@ -1,3 +1,16 @@
-public class move-all-zeroes-to-end-of-array {
-    
+class Solution {
+    void pushZerosToEnd(int[] arr, int n) {
+        int left = 0;
+        int right = 0;
+
+        while (right < n) {
+            if (arr[right] != 0) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+            }
+            right++;
+        }
+    }
 }
