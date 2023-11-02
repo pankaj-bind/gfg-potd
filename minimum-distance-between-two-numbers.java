bff814f3594af825b597ecac186892abd5a8e176
Pankaj Bind <pankajbind30@gmail.com>
2023-11-02 08:38:39 +0530

    2th updated

--- minimum-distance-between-two-numbers.java
+++ minimum-distance-between-two-numbers.java
@@ -1,3 +1,23 @@
-public class minimum-distance-between-two-numbers {
-    
+class Solution {
+    int minDist(int a[], int n, int x, int y) {
+        int minDistance = Integer.MAX_VALUE;
+        int lastSeenX = -1;
+        int lastSeenY = -1;
+        
+        for (int i = 0; i < n; i++) {
+            if (a[i] == x) {
+                if (lastSeenY != -1) {
+                    minDistance = Math.min(minDistance, i - lastSeenY);
+                }
+                lastSeenX = i;
+            } else if (a[i] == y) {
+                if (lastSeenX != -1) {
+                    minDistance = Math.min(minDistance, i - lastSeenX);
+                }
+                lastSeenY = i;
+            }
+        }
+        
+        return (minDistance == Integer.MAX_VALUE) ? -1 : minDistance;
+    }
 }
