938852acaa9cccb1a279b09d3e438c88d0b237ce
Pankaj Bind <pankajbind30@gmail.com>
2023-11-09 15:47:09 +0530

    9th nov updated

--- predict-the-column.java
+++ predict-the-column.java
@@ -1,3 +1,24 @@
-public class predict-the-column {
-    
+class Solution
+{
+    int columnWithMaxZeros(int arr[][], int N)
+    {
+        int maxZeros = 0;
+        int maxZerosColumn = -1;
+        
+        for (int j = 0; j < N; j++) {
+            int zerosInColumn = 0;
+            for (int i = 0; i < N; i++) {
+                if (arr[i][j] == 0) {
+                    zerosInColumn++;
+                }
+            }
+            
+            if (zerosInColumn > maxZeros) {
+                maxZeros = zerosInColumn;
+                maxZerosColumn = j;
+            }
+        }
+        
+        return maxZerosColumn;
+    }
 }
