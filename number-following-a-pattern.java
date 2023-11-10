aa0498bd2ee3c2da224484ee46d0fe93c80fd2bc
Pankaj Bind <pankajbind30@gmail.com>
2023-11-10 07:57:43 +0530

    10th nov updated

--- number-following-a-pattern.java
+++ number-following-a-pattern.java
@@ -1,3 +1,26 @@
-public class number-following-a-pattern {
-    
+class Solution{
+    static String printMinNumberForPattern(String S){
+        // code here
+        int n = S.length();
+        int[] result = new int[n + 1];
+        int current_max = 0;
+        int next_element = 1;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i <= n; i++) {
+            stack.push(next_element++);
+            if (i == n || S.charAt(i) == 'I') {
+                while (!stack.isEmpty()) {
+                    result[current_max++] = stack.pop();
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n + 1; i++) {
+            sb.append(result[i]);
+        }
+
+        return sb.toString();
+    }
 }
