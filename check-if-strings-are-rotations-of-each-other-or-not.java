fb0e7d1340b3c301724e28a25d488a4f427b70f4
Pankaj Bind <pankajbind30@gmail.com>
2023-11-14 09:03:23 +0530

    14th nov updated

--- check-if-strings-are-rotations-of-each-other-or-not.java
+++ check-if-strings-are-rotations-of-each-other-or-not.java
@@ -1,3 +1,16 @@
-public class check-if-strings-are-rotations-of-each-other-or-not {
-    
+class Solution
+{
+    // Function to check if two strings are rotations of each other or not.
+    public static boolean areRotations(String s1, String s2)
+    {
+        // Check if lengths of two strings are equal
+        if (s1.length() != s2.length())
+            return false;
+
+        // Concatenate s1 with itself
+        String temp = s1 + s1;
+
+        // Check if s2 is a substring of the concatenated string s1+s1
+        return temp.contains(s2);
+    }
 }
