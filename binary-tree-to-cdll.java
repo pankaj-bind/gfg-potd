dea6937ae4f526c0fb770558965a8aa190b215ca
Pankaj Kumar Bind <73558583+Pankaj-Bind@users.noreply.github.com>
2023-11-17 19:48:12 +0530

    Update binary-tree-to-cdll.java

--- binary-tree-to-cdll.java
+++ binary-tree-to-cdll.java
@@ -1,1 +1,34 @@
+class Solution
+{ 
+    //Function to convert binary tree into circular doubly linked list.
+     Node head = null, prev = null;
+    Node bTreeToClist(Node root)
+    {
+         inorder(root);
 
+        // fixing the last node
+        head.left = prev;
+        prev.right = head;
+
+        return head;
+    }
+
+    void inorder(Node curr) {
+        if (curr == null)
+            return;
+
+        inorder(curr.left);
+
+        if (head == null)
+            head = prev = curr;
+        else {
+            prev.right = curr;
+            curr.left = prev;
+        }
+
+        // before moving right update prev to curr.
+        prev = curr;
+        inorder(curr.right);
+    }
+    
+}
