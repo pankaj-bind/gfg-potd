f4b08013cccc9e80c2a869316d99e4b6f2819ec8
Pankaj Bind <pankajbind30@gmail.com>
2023-11-18 00:50:26 +0530

    18th nov updated

--- reverse-a-doubly-linked-list.java
+++ reverse-a-doubly-linked-list.java
@@ -1,3 +1,21 @@
-public class reverse-a-doubly-linked-list {
-    
+public static Node reverseDLL(Node head)
+{
+    //Your code here
+    Node current = head;
+    Node temp = null;
+
+    // Swap next and prev pointers for all nodes of the doubly linked list
+    while (current != null) {
+        temp = current.prev;
+        current.prev = current.next;
+        current.next = temp;
+        current = current.prev;
+    }
+
+    // Update the head
+    if (temp != null) {
+        head = temp.prev;
+    }
+
+    return head;
 }
