52cc83d42bf487240c27a0f042cdd0486bd6fce0
Pankaj Bind <pankajbind30@gmail.com>
2023-11-30 03:50:33 +0530

    30th nov updated

--- shortest-path-from-1-to-n.java
+++ shortest-path-from-1-to-n.java
@@ -1,3 +1,16 @@
-public class shortest-path-from-1-to-n {
-    
+class Solution {
+    public int minStep(int n) {
+        int steps = 0;
+        
+        while (n > 1) {
+            if (n % 3 == 0) {
+                n /= 3;
+            } else {
+                n -= 1;
+            }
+            steps++;
+        }
+        
+        return steps;
+    }
 }
