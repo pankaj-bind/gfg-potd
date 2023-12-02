94d2443f855cfc5c629a56f9c75e41d1a8f4192a
Pankaj Bind <pankajbind30@gmail.com>
2023-12-02 12:12:33 +0530

    2nd dec updated

--- inorder-traversal-and-bst.java
+++ inorder-traversal-and-bst.java
@@ -1,3 +1,14 @@
-public class inorder-traversal-and-bst {
-    
+class Solution
+{
+    static int isRepresentingBST(int arr[], int N)
+    {
+        // Traverse the array and check if it is sorted and no two adjacent elements are the same
+        for (int i = 0; i < N - 1; i++) {
+            if (arr[i] >= arr[i + 1]) {
+                return 0; // Not in ascending order
+            }
+        }
+        
+        return 1; // It represents an inorder traversal of a BST
+    }
 }
