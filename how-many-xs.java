92c4545f183efd764cfb0339be5d8e60a6539e7c
Pankaj Bind <pankajbind30@gmail.com>
2023-12-06 19:34:41 +0530

    6th dec updated

--- how-many-xs.java
+++ how-many-xs.java
@@ -1,3 +1,14 @@
-public class how-many-xs {
-    
-}
+class Solution {
+    int countX(int L, int R, int X) {
+        int counter =0;
+        for(int i=L+1; i<R; i++){
+            int temp = i;
+            while(temp>0){
+                int mod = temp%10;
+                if(mod==X)counter++;
+                temp/=10;
+            }
+        }
+        return counter;
+    }
+};
