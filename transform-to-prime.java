e253a36ffe23ad04326de90cf557040fae2ab809
Pankaj Bind <pankajbind30@gmail.com>
2023-12-08 08:44:21 +0530

    8th dec updated

--- transform-to-prime.java
+++ transform-to-prime.java
@@ -1,3 +1,29 @@
-public  transform-to-prime {
-    
+class Solution {
+    public int minNumber(int arr[], int N) {
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+
+        int toInsert = 0;
+        while (!isPrime(sum)) {
+            sum++;
+            toInsert++;
+        }
+
+        return toInsert;
+    }
+
+    // Function to check if a number is prime
+    private boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
