4dcb01be45ff893736b7e72537f97b0b80039ab6
Pankaj Bind <pankajbind30@gmail.com>
2023-12-10 11:21:18 +0530

    10th dec updated

--- subarray-with-0-sum.java
+++ subarray-with-0-sum.java
@@ -1,3 +1,24 @@
-public class subarray-with-0-sum {
-    
+class Solution {
+    // Function to check whether there is a subarray present with 0-sum or not.
+    static boolean findsum(int arr[], int n) {
+        // Your code here
+        Set<Integer> set = new HashSet<>();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+
+            // If the current sum is already present in the set, then there is a subarray
+            // with sum 0.
+            if (set.contains(sum) || sum == 0) {
+                return true;
+            }
+
+            // Add the current sum to the set.
+            set.add(sum);
+        }
+
+        // If we reach here, then no subarray with sum 0 is found.
+        return false;
+    }
 }
