a3b70c82ba30eef5beb4752cacb5cc19e6824aab
Pankaj Bind <pankajbind30@gmail.com>
2023-12-11 19:32:23 +0530

    11th dec updated

--- max-sum-subarray-of-size-k.java
+++ max-sum-subarray-of-size-k.java
@@ -1,3 +1,28 @@
-public class max-sum-subarray-of-size-k {
-    
+class Solution {
+    static long maximumSumSubarray(int K, ArrayList<Integer> Arr, int N) {
+        // code here
+        long maxSum = 0;
+        long currentSum = 0;
+
+        // Calculate sum of the first K elements
+        for (int i = 0; i < K; i++) {
+            currentSum += Arr.get(i);
+        }
+
+        maxSum = currentSum;
+
+        // Iterate through the array using sliding window
+        for (int i = K; i < N; i++) {
+            // Include the current element in the window
+            currentSum += Arr.get(i);
+
+            // Exclude the leftmost element from the window
+            currentSum -= Arr.get(i - K);
+
+            // Update the maximum sum if the current sum is greater
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
 }
