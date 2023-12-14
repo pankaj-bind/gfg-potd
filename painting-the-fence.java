d84c5d6c80fdba0400943a26a3f3f125ce8a929b
Pankaj Kumar Bind <pankajbind30@gmail.com>
2023-12-14 21:09:16 +0530

    14th dec updated

--- painting-the-fence.java
+++ painting-the-fence.java
@@ -1,3 +1,18 @@
-public class painting-the-fence {
-    
+class Solution
+{
+    long countWays(int n,int k)
+    {
+        //code here.
+        long mod = 1000000007;
+        long same = 0, diff = k, total = same+diff;
+        
+        for(int i=2; i<=n; i++){
+            same = diff;
+            diff = (total*(k-1))%mod;
+            total = (same+diff)%mod;
+            
+        }
+        return total;
+        
+    }
 }
