3af2ffaf36cea9dcb2220faa8f5f07e3770ed67a
Pankaj Kumar Bind <pankajbind30@gmail.com>
2023-12-18 01:11:30 +0530

    18th dec updated

--- game-of-xor.java
+++ game-of-xor.java
@@ -1,3 +1,18 @@
-public class game-of-xor {
-    
+class Solution {
+    static int gameOfXor(int N , int[] A) {
+        int result = 0;
+        
+        for (int i = 0; i < N; i++) {
+            // Count the number of subarrays containing A[i]
+            // Formula: (i + 1) * (N - i)
+            int count = (i + 1) * (N - i);
+            
+            // If the count is odd, XOR the result with A[i]
+            if (count % 2 == 1) {
+                result ^= A[i];
+            }
+        }
+        
+        return result;
+    }
 }
