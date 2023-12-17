11505e7ca3ee4746782990c9ed408b407fe850af
Pankaj Kumar Bind <pankajbind30@gmail.com>
2023-12-17 10:41:33 +0530

    17th dec updated

--- max-sum-without-adjacents.java
+++ max-sum-without-adjacents.java
@@ -1,3 +1,20 @@
-public class max-sum-without-adjacents {
-    
+class Solution {
+    int findMaxSum(int arr[], int n) {
+        if (n == 0) {
+            return 0;
+        }
+
+        int incl = arr[0]; // Maximum sum including the first element
+        int excl = 0;      // Maximum sum excluding the first element
+
+        for (int i = 1; i < n; i++) {
+            // Update incl and excl for the current element
+            int newIncl = excl + arr[i];
+            excl = Math.max(incl, excl);
+            incl = newIncl;
+        }
+
+        // Return the maximum of incl and excl
+        return Math.max(incl, excl);
+    }
 }
