115fc9213916fab81b7bcd1bce92cf40bc85576e
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-01-02 12:28:47 +0530

    2nd jan updated

--- largest-sum-subarray-of-size-at-least-k.java
+++ largest-sum-subarray-of-size-at-least-k.java
@@ -1,3 +1,23 @@
-public class largest-sum-subarray-of-size-at-least-k {
+class Solution {
     
+    public long maxSumWithK(long a[], long n, long k)
+    {
+        long ans=Integer.MIN_VALUE,sum1=0,sum2=0;
+        int j=0;
+        for(int i=0;i<n;i++){
+            sum1+=a[i];
+            if(i-j+1==k){
+                ans=Math.max(sum1,ans);
+            }
+            else if(i-j+1>k){
+                sum2+=a[j++];
+                if(sum2<0){
+                    sum1-=sum2;
+                    sum2=0;
+                }
+                ans=Math.max(ans,sum1);
+            }
+        }
+        return ans;
+    }
 }
