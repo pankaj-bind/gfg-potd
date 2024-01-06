960a5c001009b5ccbc94c38b3b91b52357357ad7
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-01-06 12:09:49 +0530

    6th jan updated

--- techfest-and-the-queue.java
+++ techfest-and-the-queue.java
@@ -1,3 +1,28 @@
-public class techfest-and-the-queue {
-    
+
+class Solution {
+    public static long sumOfPowers(long a, long b) {
+        // code here
+        int[] spf = new int[(int)b+1];
+        Arrays.fill(spf, -1);
+        if(b==1)
+            return 0;
+        if(a==1)
+            a++;
+        for(int i=2; i<=b; i++){
+            if(spf[i]== -1){
+                for(int j=i; j<=b; j+=i)
+                    spf[j]=i;
+            }
+        }
+        long ans = 0;
+        for(int i = (int)a; i<=b; i++){
+            int x=i;
+            while(x!=1){
+                x/=spf[x];
+                ans++;
+            }
+        }
+        return ans;
+    }
 }
+  
