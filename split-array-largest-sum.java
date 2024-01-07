7bfa4fc6e7d8852962ab07c0dc9174332578f9f4
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-01-07 12:11:45 +0530

    7th jan updated

--- split-array-largest-sum.java
+++ split-array-largest-sum.java
@@ -1,3 +1,34 @@
-public class split-array-largest-sum {
-    
+class Solution {
+    public static int splitArray(int[] arr, int N, int K) {
+        int low = Arrays.stream(arr).max().getAsInt(); // Minimum possible maximum subarray sum
+        int high = Arrays.stream(arr).sum(); // Maximum possible maximum subarray sum
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (isValid(arr, N, K, mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+
+    private static boolean isValid(int[] arr, int N, int K, int mid) {
+        int count = 1;
+        int sum = 0;
+
+        for (int num : arr) {
+            sum += num;
+
+            if (sum > mid) {
+                count++;
+                sum = num;
+            }
+        }
+
+        return count <= K;
+    }
 }
