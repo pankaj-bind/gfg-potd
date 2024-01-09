1442b1ad29cdfae0b42b2c9e3db19c7499dbfcbe
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-01-09 08:45:57 +0530

    9th jab updated

--- search-pattern-kmp-algorithm.java
+++ search-pattern-kmp-algorithm.java
@@ -1,3 +1,56 @@
-public class search-pattern-kmp-algorithm {
-    
+class Solution {
+
+    ArrayList<Integer> search(String pat, String txt) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        int M = pat.length();
+        int N = txt.length();
+
+        // Create lps[] that will hold the longest prefix suffix
+        // values for pattern
+        int[] lps = new int[M];
+        computeLPSArray(pat, M, lps);
+
+        int i = 0; // index for txt[]
+        int j = 0; // index for pat[]
+        while (i < N) {
+            if (pat.charAt(j) == txt.charAt(i)) {
+                i++;
+                j++;
+            }
+
+            if (j == M) {
+                result.add(i - j + 1);
+                j = lps[j - 1];
+            } else if (i < N && pat.charAt(j) != txt.charAt(i)) {
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i = i + 1;
+            }
+        }
+
+        return result;
+    }
+
+    void computeLPSArray(String pat, int M, int[] lps) {
+        int len = 0;
+        int i = 1;
+        lps[0] = 0;
+
+        while (i < M) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
 }
