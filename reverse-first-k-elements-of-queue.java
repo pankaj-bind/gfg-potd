b88a71c0e7ff29efa7b598464c686ad912c01bdb
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-01-12 19:11:21 +0530

    12th jan updated

--- reverse-first-k-elements-of-queue.java
+++ reverse-first-k-elements-of-queue.java
@@ -1,3 +1,24 @@
-public class reverse-first-k-elements-of-queue {
-    
+class GfG {
+    // Function to reverse first k elements of a queue.
+    public Queue<Integer> modifyQueue(Queue<Integer> q, int k) {
+        // Using a stack to reverse the first K elements
+        Stack<Integer> stack = new Stack<>();
+
+        // Push the first K elements into the stack
+        for (int i = 0; i < k; i++) {
+            stack.push(q.poll());
+        }
+
+        // Enqueue the elements from the stack back into the queue
+        while (!stack.isEmpty()) {
+            q.add(stack.pop());
+        }
+
+        // Enqueue the remaining elements back into the queue
+        for (int i = 0; i < q.size() - k; i++) {
+            q.add(q.poll());
+        }
+
+        return q;
+    }
 }
