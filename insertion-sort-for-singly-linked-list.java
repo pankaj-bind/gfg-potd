443d2e9bc6087107151740d202eb9f212d9b881d
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-01-13 21:16:36 +0530

    13th jan updated

--- insertion-sort-for-singly-linked-list.java
+++ insertion-sort-for-singly-linked-list.java
@@ -1,3 +1,31 @@
-public class insertion-sort-for-singly-linked-list {
-    
+class Solution {
+    public static Node insertionSort(Node head_ref) {
+        // code here
+        if (head_ref == null || head_ref.next == null) {
+            return head_ref;
+        }
+
+        Node sorted = null;
+        Node current = head_ref;
+
+        while (current != null) {
+            Node next = current.next;
+
+            if (sorted == null || sorted.data >= current.data) {
+                current.next = sorted;
+                sorted = current;
+            } else {
+                Node temp = sorted;
+                while (temp.next != null && temp.next.data < current.data) {
+                    temp = temp.next;
+                }
+                current.next = temp.next;
+                temp.next = current;
+            }
+
+            current = next;
+        }
+
+        return sorted;
+    }
 }
