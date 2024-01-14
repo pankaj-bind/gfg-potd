50aec96fae27939ed9402059140c420b43583dd4
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-01-14 12:26:06 +0530

    14th jan updated

--- find-duplicate-rows-in-a-binary-matrix.java
+++ find-duplicate-rows-in-a-binary-matrix.java
@@ -1,3 +1,25 @@
-public class find-duplicate-rows-in-a-binary-matrix {
-    
+class Solution {
+    public static ArrayList<Integer> repeatedRows(int matrix[][], int m, int n) {
+        ArrayList<Integer> result = new ArrayList<>();
+        Set<String> seenRows = new HashSet<>();
+
+        for (int i = 0; i < m; i++) {
+            StringBuilder rowStringBuilder = new StringBuilder();
+
+            for (int j = 0; j < n; j++) {
+                rowStringBuilder.append(matrix[i][j]);
+            }
+
+            String rowString = rowStringBuilder.toString();
+
+            if (seenRows.contains(rowString)) {
+                // This row is a duplicate
+                result.add(i);
+            } else {
+                seenRows.add(rowString);
+            }
+        }
+
+        return result;
+    }
 }
