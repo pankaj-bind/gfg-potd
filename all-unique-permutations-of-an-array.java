ccf5a44ac3b711b77bead9de7b369efe80ee44b8
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-01-17 19:25:32 +0530

    17th jan updated

--- all-unique-permutations-of-an-array.java
+++ all-unique-permutations-of-an-array.java
@@ -1,3 +1,37 @@
-public class all-unique-permutations-of-an-array {
-    
+class Solution {
+    static ArrayList<ArrayList<Integer>> uniquePerms(ArrayList<Integer> arr, int n) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        boolean[] used = new boolean[n];
+        ArrayList<Integer> currentPermutation = new ArrayList<>();
+
+        // Sort the input array to ensure unique permutations are in sorted order
+        Collections.sort(arr);
+
+        generatePermutations(arr, n, used, currentPermutation, result);
+
+        return result;
+    }
+
+    static void generatePermutations(ArrayList<Integer> arr, int n, boolean[] used,
+                                      ArrayList<Integer> currentPermutation,
+                                      ArrayList<ArrayList<Integer>> result) {
+        if (currentPermutation.size() == n) {
+            result.add(new ArrayList<>(currentPermutation));
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (used[i] || (i > 0 && arr.get(i).equals(arr.get(i - 1)) && !used[i - 1])) {
+                continue;
+            }
+
+            used[i] = true;
+            currentPermutation.add(arr.get(i));
+
+            generatePermutations(arr, n, used, currentPermutation, result);
+
+            used[i] = false;
+            currentPermutation.remove(currentPermutation.size() - 1);
+        }
+    }
 }
