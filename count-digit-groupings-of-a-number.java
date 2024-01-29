caceb7bf79b5b9b74ff3e2db229dbee9a3d3df7a
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-01-29 19:24:46 +0530

    28th jan updated

--- count-digit-groupings-of-a-number.java
+++ count-digit-groupings-of-a-number.java
@@ -1,3 +1,24 @@
-public class count-digit-groupings-of-a-number {
-    
+class Solution
+{
+    static int help(String str,int index,int preSum,int dp[][]){
+	    if(index==str.length())return 1;
+	    if(dp[index][preSum]!=-1)return dp[index][preSum];
+	    int curSum=0,ans=0;
+	    for(int i=index;i<str.length();i++){
+	        curSum+=str.charAt(i)-'0';
+	        if(curSum>=preSum){
+	            ans+=help(str,i+1,curSum,dp);
+	        }
+	        
+	    }
+	    return dp[index][preSum]=ans;
+	}
+    public int TotalCount(String str)
+    {
+        // Code here
+        int n=str.length();
+	    int dp[][] = new int[n][901];
+	    for(int temp[]:dp)Arrays.fill(temp,-1);
+	    return help(str,0,0,dp);
+    }
 }
