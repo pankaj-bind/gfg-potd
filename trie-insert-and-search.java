5e7234a72a75241c1f7d3a828032fee50eeb6b79
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-01-31 19:06:44 +0530

    31st jan updated

--- trie-insert-and-search.java
+++ trie-insert-and-search.java
@@ -1,3 +1,26 @@
-public class trie-insert-and-search {
-    
+static void insert(TrieNode root, String key) {
+    TrieNode current = root;
+    int n = key.length();
+    for (int i = 0; i < n; i++) {
+        int index = key.charAt(i) - 'a';
+        if (current.children[index] == null) {
+            current.children[index] = new TrieNode();
+        }
+        current = current.children[index];
+    }
+    current.isEndOfWord = true;
+}
+
+// Function to use TRIE data structure and search the given string.
+static boolean search(TrieNode root, String key) {
+    TrieNode current = root;
+    int n = key.length();
+    for (int i = 0; i < n; i++) {
+        int index = key.charAt(i) - 'a';
+        if (current.children[index] == null) {
+            return false;
+        }
+        current = current.children[index];
+    }
+    return current != null && current.isEndOfWord;
 }
