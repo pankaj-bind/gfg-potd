69a17db409074ceebc73f424cd4ebfacdcca3491
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-03 21:27:16 +0530

    3rd feb updated

--- decimal-equivalent-of-binary-linked-list.java
+++ decimal-equivalent-of-binary-linked-list.java
@@ -1,3 +1,13 @@
-public class decimal-equivalent-of-binary-linked-list {
-    
+class Solution
+{
+    long mod = 1000000007;
+   long DecimalValue(Node head)
+   {
+        long ans=0;
+ 	    for(Node curr=head; curr!=null;  curr=curr.next)
+ 	       ans = ((ans*2)%mod + curr.data)%mod;
+
+ 	   return ans;
+   }
+   
 }
