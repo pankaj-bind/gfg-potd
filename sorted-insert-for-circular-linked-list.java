474d5f8336cfda049cd0a4fd41364b0fd61e8120
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-05 18:49:52 +0530

    5th feb updated

--- sorted-insert-for-circular-linked-list.java
+++ sorted-insert-for-circular-linked-list.java
@@ -1,3 +1,40 @@
-public class sorted-insert-for-circular-linked-list {
-    
+class Solution {
+    public Node sortedInsert(Node head, int data) {
+        Node newNode = new Node(data);
+
+        // Case 1: If the linked list is empty
+        if (head == null) {
+            newNode.next = newNode; // Point to itself
+            return newNode; // Set new node as the head
+        }
+
+        // Case 2: If the new node should be inserted before the head
+        if (data <= head.data) {
+            Node last = getLastNode(head);
+            newNode.next = head;
+            last.next = newNode; // Update last node's next to the new node
+            return newNode; // Set new node as the head
+        }
+
+        // Case 3: Traverse the linked list to find the appropriate position to insert
+        Node current = head;
+        while (current.next != head && current.next.data < data) {
+            current = current.next;
+        }
+
+        // Insert the new node after the current node
+        newNode.next = current.next;
+        current.next = newNode;
+
+        return head;
+    }
+
+    // Helper function to get the last node in the circular linked list
+    private Node getLastNode(Node head) {
+        Node temp = head;
+        while (temp.next != head) {
+            temp = temp.next;
+        }
+        return temp;
+    }
 }
