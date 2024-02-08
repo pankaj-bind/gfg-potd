317776244985687b69f104b1488877b92c086a1c
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-08 20:44:18 +0530

    8th feb updated

--- check-if-all-leaves-are-at-same-level.java
+++ check-if-all-leaves-are-at-same-level.java
@@ -1,3 +1,37 @@
-public class check-if-all-leaves-are-at-same-level {
-    
+class Solution
+{
+    boolean check(Node root)
+    {
+	// Your code here
+	   Queue<Node> q=new LinkedList<>();
+	   q.add(root);
+	   int i=0;
+	   int last=-1;
+	   while(!q.isEmpty()){
+	       
+	       int len=q.size();
+	       
+	       
+	       for(int j=0;j<len;j++){
+	           Node node=q.poll();
+    	       
+    	           if(node.left!=null){
+    	               q.add(node.left);
+    	           }
+    	           if(node.right!=null){
+    	               q.add(node.right);
+    	           }
+	           
+	           if(node.left==null && node.right==null){
+	               if(last==-1 ) last=i;
+	               else if(last!=i) return false;
+	           }
+	           
+	       }
+	       i++;
+	      
+	   }
+	   
+	   return true;
+    }
 }
