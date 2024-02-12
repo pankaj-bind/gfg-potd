d31577ae301055698836e9e53fe4b2f89f452e85
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-12 20:21:41 +0530

    12th feb updated

--- recursive-sequence.java
+++ recursive-sequence.java
@@ -1,3 +1,18 @@
-public class recursive-sequence {
-    
+class Solution{
+    static long mod=1000000007;
+    static long sequence(int n){
+         long ans = 0;
+         long aa = 1;
+        for(int i=0; i<n;i++){
+           long a =1;
+           for(int j = 0; j<=i; j++){
+               a *= aa;
+               a %=mod;
+               aa = aa+1;
+           }
+           ans += a;
+           ans %= mod;
+        }
+        return ans;
+    }
 }
