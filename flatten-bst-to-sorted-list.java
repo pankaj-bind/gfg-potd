58ec9f0989fed9016a26793fe70b23d3cfeb31b1
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-16 23:33:38 +0530

    16th feb updated

--- flatten-bst-to-sorted-list.java
+++ flatten-bst-to-sorted-list.java
@@ -1,3 +1,23 @@
-public class flatten-bst-to-sorted-list {
-    
+class Solution 
+{
+    public Node flattenBST(Node root) 
+    {
+        if(root==null||(root.left==null&&root.right==null))
+        {
+            return root;
+        }
+        Node l=flattenBST(root.left);
+        Node r=flattenBST(root.right);
+         root.left=null;
+         root.right=r;
+         if(l==null)return root; 
+         else 
+         {  
+            Node temp=l;
+            while(temp.right!=null) temp=temp.right;
+            temp.right=root;
+            root=l;
+            return root;
+         }
+    }
 }
