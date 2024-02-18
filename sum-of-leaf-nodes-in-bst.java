c857bed4963b4c741d2a19ff0e50d8b1716aa6b7
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-18 17:23:52 +0530

    18th feb updated

--- sum-of-leaf-nodes-in-bst.java
+++ sum-of-leaf-nodes-in-bst.java
@@ -1,3 +1,13 @@
-public class sum-of-leaf-nodes-in-bst {
-    
+class Solution
+{
+    public static int sumOfLeafNodes(Node root)
+    {
+        if(root == null){
+            return 0;
+        }
+        if(root.left== null && root.right==null){
+            return root.data;
+        }
+        return sumOfLeafNodes(root.left)+sumOfLeafNodes(root.right);
+    }
 }
