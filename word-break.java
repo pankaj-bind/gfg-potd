a9673825ccf6e9f12e5ee0b30e6022bd6fb6db8c
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-20 19:07:44 +0530

    20th feb updated

--- word-break.java
+++ word-break.java
@@ -1,3 +1,19 @@
-public class word-break {
-    
+class Solution {
+    public static int wordBreak(int n, String s, ArrayList<String> dictionary) {
+        // code here
+        int len = s.length();
+        boolean[] dp = new boolean[len + 1];
+        dp[0] = true; // empty string is always a valid segmentation
+
+        for (int i = 1; i <= len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && dictionary.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[len] ? 1 : 0;
+    }
 }
