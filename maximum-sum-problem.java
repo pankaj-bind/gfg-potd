e7074e9e1f902839d407b9c2ab3f890cbb3b34b1
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-24 19:22:11 +0530

    24 feb updated

--- maximum-sum-problem.java
+++ maximum-sum-problem.java
@@ -1,3 +1,11 @@
-public class maximum-sum-problem {
-    
+class Solution
+{
+    public int maxSum(int n) 
+    { 
+        if(n==0 || n==1){
+            return n;
+        }
+        
+        return Math.max(maxSum(n/2)+maxSum(n/3)+maxSum(n/4),n);
+    } 
 }
