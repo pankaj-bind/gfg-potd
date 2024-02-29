47c79e088932fcdda1cf2b3ac6a095e0aa7fc60a
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-29 19:36:16 +0530

    29th feb updated

--- sum-of-bit-differences.java
+++ sum-of-bit-differences.java
@@ -1,3 +1,19 @@
-public class sum-of-bit-differences {
-    
-}
+
+class Solution {
+    long sumBitDifferences(int[] arr, int n) {
+         // code here
+         long num=1,ans=0,hai=0,nhiHai=0;
+         for(int i=0;i<21;i++){
+             for(int j=0;j<n;j++){
+                 if((arr[j]&num)!=0)hai++;
+                 else nhiHai++;
+             }
+             ans+=2*hai*nhiHai;
+             hai=0;
+             nhiHai=0;
+             num<<=1;
+         }
+         return ans;
+     }
+ }
+ 
