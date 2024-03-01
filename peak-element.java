0049af3cd4190805a8c73151749a59940ace034d
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-03-01 19:49:33 +0530

    1st march updated

--- peak-element.java
+++ peak-element.java
@@ -1,3 +1,22 @@
-public class peak-element {
-    
+class Solution
+{
+	// Function to find the peak element
+	// arr[]: input array
+	// n: size of array a[]
+	public int peakElement(int[] arr,int n)
+    {
+       int start = 0;
+        int end = arr.length-2;
+       
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]>arr[mid+1]){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return start;
+    }
 }
