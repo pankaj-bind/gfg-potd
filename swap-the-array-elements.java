1cc3f85e88ed741cff8ff69a35a191af422bac40
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-03-04 18:29:02 +0530

    4th mar updated

--- swap-the-array-elements.java
+++ swap-the-array-elements.java
@@ -1,3 +1,12 @@
-public class swap-the-array-elements {
-    
+class Solution
+{
+    public void swapElements(int[] arr, int n)
+    {
+        // Code here
+        for(int i=0;i<n-2;i++){
+            int temp = arr[i];
+            arr[i]=arr[i+2];
+            arr[i+2]=temp;
+        }
+    }
 }
