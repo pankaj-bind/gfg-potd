3370cd6051190f2be728bdabe5897584a08990fd
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-03-05 21:24:00 +0530

    5th mar updated

--- maximum-index.java
+++ maximum-index.java
@@ -1,3 +1,29 @@
-public class maximum-index {
+class Solution{
     
+    // A[]: input array
+    // N: size of array
+    // Function to find the maximum index difference.
+    static int maxIndexDiff(int a[], int n) { 
+        
+        // Your code here
+        Stack<Integer> st=new Stack<>();
+        st.push(n-1);
+        for(int i=n-2; i>=0; i--){
+            if(a[i]>a[st.peek()]){
+                st.push(i);
+            }
+        }
+        int low=0;
+        int ans=0;
+        
+        while(!st.isEmpty() && low<n){
+            if(a[st.peek()]>=a[low]){
+                ans=Math.max(ans, st.pop()-low);
+            }
+            else{
+                low++;
+            }
+        }
+        return ans;
+    }
 }
