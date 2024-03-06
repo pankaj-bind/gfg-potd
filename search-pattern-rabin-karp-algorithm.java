e4c3b9057dd23581dd954b9d04d7f4e88f10d641
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-03-06 21:31:23 +0530

    6th mar updated

--- search-pattern-rabin-karp-algorithm.java
+++ search-pattern-rabin-karp-algorithm.java
@@ -1,3 +1,26 @@
-public class search-pattern-rabin-karp-algorithm {
+class Solution
+{
     
+    ArrayList<Integer> search(String pattern, String text)
+    {
+        // your code here
+       StringBuilder s=new StringBuilder(text);
+       ArrayList<Integer> a=new ArrayList<>();
+       int ans=0;
+       int co=0;
+       while(ans!=-1)
+       {
+           ans=s.indexOf(pattern,ans);
+           if(ans!=-1)
+           {
+               a.add(ans+co+1);
+               s.deleteCharAt(ans);
+               co++;
+           }
+
+
+       }
+
+        return a;
+    }
 }
