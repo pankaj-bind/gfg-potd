c78d10323eb9038f220f320fcfce169ab265ce06
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-03-07 20:32:32 +0530

    7th mar updated

--- longest-repeating-and-non-overlapping-substring.java
+++ longest-repeating-and-non-overlapping-substring.java
@@ -1,3 +1,19 @@
-public class longest-repeating-and-non-overlapping-substring {
-    
-}
+class Solution {
+    static String longestSubstring(String s, int n) {
+        // code here
+        String str="";
+        int i=0, j=0;
+        while(i<n && j<n){
+            String substr = s.substring(i,j+1);
+            if(s.substring(j+1,n).indexOf(substr)!=-1)
+            {
+                if(str.length()<(j-i+1))
+                str=substr;
+            }
+            else
+            i++;
+            j++;
+        }
+        return str==""?"-1":str;
+    }
+};
