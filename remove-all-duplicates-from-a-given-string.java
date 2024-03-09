b52bfd605ab5a1a3137f01875c2be67bcd7c5e8f
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-03-10 00:47:15 +0530

    10th mar updated

--- remove-all-duplicates-from-a-given-string.java
+++ remove-all-duplicates-from-a-given-string.java
@@ -1,3 +1,19 @@
-public remove-all-duplicates-from-a-given-string {
-    
+class Solution {
+    String removeDuplicates(String str) {
+        // Using LinkedHashSet to maintain order and remove duplicates
+        LinkedHashSet<Character> set = new LinkedHashSet<>();
+        
+        // Traverse the string and add characters to the set
+        for (char ch : str.toCharArray()) {
+            set.add(ch);
+        }
+        
+        // Construct the resultant string from the set
+        StringBuilder result = new StringBuilder();
+        for (char ch : set) {
+            result.append(ch);
+        }
+        
+        return result.toString();
+    }
 }
