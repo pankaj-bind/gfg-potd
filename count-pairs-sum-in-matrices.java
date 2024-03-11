346a910a52c2f82c786dade5e771d94382adeaab
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-03-11 19:50:42 +0530

    11th mar updated

--- count-pairs-sum-in-matrices.java
+++ count-pairs-sum-in-matrices.java
@@ -1,3 +1,20 @@
-public class count-pairs-sum-in-matrices {
-    
+class Solution {
+    int countPairs(int mat1[][], int mat2[][], int n, int x) {
+       int count=0;
+       HashSet<Integer> set1=new HashSet<>();
+       
+       for(int i=0;i<n;i++){
+           for(int j=0;j<n;j++){
+               set1.add(mat1[i][j]);
+           }
+       }
+       for(int i=0;i<n;i++){
+           for(int j=0;j<n;j++){
+               if(set1.contains(x-mat2[i][j])){
+                   count++;
+               }
+           }
+       }
+       return count;
+    }
 }
