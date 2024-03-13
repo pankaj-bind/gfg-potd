a8246c89530d3450da614c497341ca3a3ca7bda3
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-03-13 12:13:23 +0530

    13th mar updated

--- print-matrix-in-diagonal-pattern.java
+++ print-matrix-in-diagonal-pattern.java
@@ -1,3 +1,30 @@
-public class print-matrix-in-diagonal-pattern {
-    
-}
+class Solution {
+    public int[] matrixDiagonally(int[][] mat) {
+         int row=0,col=0,n=mat.length;
+         boolean isReverse=false;
+         int ans[] = new int[n*n];
+         int index=0;
+         while(col<n){
+             ArrayList<Integer> temp = new ArrayList<>();
+             int i=row,j=col;
+             while(i<n && j<n && i>=0 && j>=0){
+                 temp.add(mat[i][j]);
+                 i--;
+                 j++;
+             }
+             if(isReverse)Collections.reverse(temp);
+             for(int k=0;k<temp.size();k++){
+                 ans[index++]=temp.get(k);
+             }
+             if(row!=n-1){
+                 row++;
+                 col=0;
+             }
+             else{
+                 col++;
+             }
+             isReverse=!isReverse;
+         }
+         return ans;
+    }
+};
