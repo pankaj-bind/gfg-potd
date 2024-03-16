b60ffabed51d71974dcf982b1c0c64ba86dfa488
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-03-16 12:15:00 +0530

    16th mar updated

--- delete-without-head-pointer.java
+++ delete-without-head-pointer.java
@@ -1,3 +1,12 @@
-public class delete-without-head-pointer {
-    
+//Function to delete a node without any reference to head pointer.
+class Solution
+{
+    void deleteNode(Node del_node)
+    {
+        // Copy the value of the next node to the node to be deleted.
+        del_node.data = del_node.next.data;
+        
+        // Point the next pointer of the node to be deleted to the next of the next node.
+        del_node.next = del_node.next.next;
+    }
 }
