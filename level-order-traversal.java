c6215ceea97194390cd4a1d4b4efa1c57268ccb2
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-03-18 01:05:56 +0530

    18th mar updated

--- level-order-traversal.java
+++ level-order-traversal.java
@@ -1,3 +1,23 @@
-public class level-order-traversal {
-    
+class Solution {
+    // Function to return the level order traversal of a tree.
+    static ArrayList<Integer> levelOrder(Node root) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (root == null)
+            return result;
+        
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            result.add(current.data);
+            
+            if (current.left != null)
+                queue.add(current.left);
+            if (current.right != null)
+                queue.add(current.right);
+        }
+        
+        return result;
+    }
 }
