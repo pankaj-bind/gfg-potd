529a16a3da6348f419c4e25aa36cf3bcfeb327b4
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-03-23 19:59:01 +0530

    23rd mar updated

--- fibonacci-series-up-to-nth-term.java
+++ fibonacci-series-up-to-nth-term.java
@@ -1,3 +1,18 @@
-public class fibonacci-series-up-to-nth-term {
-    
+class Solution {
+
+    int[] Series(int n) {
+        // code here
+        int[] ans=new int[n+1];
+        int mod=1000000007;
+        
+        ans[0]=0;
+        ans[1]=1;
+        for(int i=2; i<=n;i++){
+            int temp=ans[i-1]+ans[i-2];
+            ans[i]=temp%mod;
+            
+        }
+        
+        return ans;
+    }
 }
