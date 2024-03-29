e72f98642d607e346f72d3a9fc3b3cc5de425084
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-03-29 21:17:03 +0530

    29th mar updated

--- euler-circuit-in-a-directed-graph.java
+++ euler-circuit-in-a-directed-graph.java
@@ -1,3 +1,11 @@
-public class euler-circuit-in-a-directed-graph {
-    
+class Solution {
+    public boolean isEularCircuitExist(int v, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        for(int i = 0; i < v; ++i){
+            if(adj.get(i).size() % 2 != 0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
