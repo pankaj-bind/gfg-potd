5e0a4c85b4027042da26bbf31c39097eb5142afd
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-03-30 19:50:31 +0530

    30th mar updated

--- minimum-element-in-bst.java
+++ minimum-element-in-bst.java
@@ -1,3 +1,13 @@
-public class minimum-element-in-bst {
-    
+class Solution {
+    // Function to find the minimum element in the given BST.
+    int minValue(Node root) {
+        if (root == null)
+            return -1; // Return -1 if the tree is empty
+        
+        // Traverse to the leftmost node
+        while (root.left != null)
+            root = root.left;
+        
+        return root.data; // Return the data of the leftmost node
+    }
 }
