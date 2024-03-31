803dfe91d2914dfaf4b4996699130cf8cb5c9bff
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-03-31 18:16:47 +0530

    31st mar updated

--- closest-neighbor-in-bst.java
+++ closest-neighbor-in-bst.java
@@ -1,3 +1,17 @@
-public class closest-neighbor-in-bst {
-    
+class Solution {
+    public static int findMaxForN(Node root, int n) {
+        // Add your code here.
+        int ans = Integer.MIN_VALUE;
+        while(root!=null){
+            if(root.key==n)return n;
+            else if(root.key<n){
+                ans=Math.max(ans,root.key);
+                root=root.right;
+            }
+            else{
+                root=root.left;
+            }
+        }
+        return ans==Integer.MIN_VALUE?-1:ans;
+    }
 }
