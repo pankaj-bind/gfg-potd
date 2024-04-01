927cf3a8d6b9bae4450c0f54e1e68daa695531b7
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-04-01 21:37:32 +0530

    1st apr updated

--- pairs-violating-bst-property.java
+++ pairs-violating-bst-property.java
@@ -1,3 +1,28 @@
-public class pairs-violating-bst-property {
-    
+class Solution {
+    static int k;
+    public static int pairsViolatingBST(int n, Node root) {
+        // code here
+      int cnt=0;
+       k=0;
+        int arr[] = new int[n];
+         inorder(root, arr);
+         
+        for(int i=0; i<n ;i++){
+            for(int j=i+1; j<n; j++){
+                if(arr[i] > arr[j])
+                cnt++;
+            }
+        }
+        return cnt;
+        
+    }
+    static void inorder(Node root, int arr[]) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left, arr);
+       arr[k++]= root.data;
+        inorder(root.right, arr);
+    }
 }
