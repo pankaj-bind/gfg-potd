71533ca7605c61fc1a6b06f617e757bfd41483f4
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-04-02 23:19:40 +0530

    2nd apr updated

--- minimum-absolute-difference-in-bst.java
+++ minimum-absolute-difference-in-bst.java
@@ -1,3 +1,26 @@
-public class minimum-absolute-difference-in-bst {
-    
+class Solution
+{
+      ArrayList<Integer> al;
+    int absolute_diff(Node root)
+    {
+       int ans = Integer.MAX_VALUE;
+        al  = new ArrayList<>();
+         inorder(root);
+         
+         for(int i=0; i<al.size()-1; i++){
+             ans = Math.min(ans, al.get(i+1) - al.get(i));
+         }
+         return ans;
+         
+    }
+     void inorder(Node root ) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+       al.add(root.data);
+        inorder(root.right);
+    }
 }
+
