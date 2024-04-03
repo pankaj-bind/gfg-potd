c1b5fa00a2015c7313b5ae903f0d375d30bf5bf3
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-04-03 20:04:03 +0530

    3rd apr updated

--- kth-common-ancestor-in-bst.java
+++ kth-common-ancestor-in-bst.java
@@ -1,3 +1,39 @@
-public class kth-common-ancestor-in-bst {
-    
+class Solution {
+    public int kthCommonAncestor(Node root, int k, int x, int y) {
+        // code here
+        Node LCA = lca(root, x, y);
+       
+        ArrayList<Integer> al = new  ArrayList<>();
+       
+        search(root, LCA, al);
+        return (al.size()-k<0) ? -1 : al.get(al.size()-k);
+    }
+    void search(Node root, Node x, ArrayList<Integer> al){ //logn
+        while(root!=null){
+             al.add(root.data);
+            if(root.data==x.data){
+            return;
+            }
+            if(root.data < x.data){
+                root = root.right;
+            }
+            else 
+            root= root.left;
+        }
+    }
+    Node lca(Node root, int n1, int n2) // ht of tree // logn
+	{
+       while (root != null) {
+            
+            if (root.data > n1 && root.data > n2) {
+                root = root.left;
+            }
+            else if (root.data < n1 && root.data < n2) {
+                root = root.right;
+            }
+            else break;
+        }
+        return root;
+    }
+
 }
