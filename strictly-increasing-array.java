7b0720d1a8fdff48de1cf493e759c9e97e4fbf3f
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-04-05 19:47:01 +0530

    5th apr updated

--- strictly-increasing-array.java
+++ strictly-increasing-array.java
@@ -1,3 +1,19 @@
-public class strictly-increasing-array {
-    
+class Solution
+{
+    public int min_operations(int []nums)
+    {
+        // Code here
+        int n=nums.length,temp=1;
+        int dp[]=new int[n];
+        Arrays.fill(dp,1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]-nums[j]>=i-j){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                    temp=Math.max(dp[i],temp);
+                }
+            }
+        }
+        return n-temp;
+    }
 }
