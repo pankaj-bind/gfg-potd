5f6bf3536927fff66fd7ac4dce760dd71d80762a
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-04-06 22:32:43 +0530

    6th apr updated

--- count.java
+++ count.java
@@ -1,3 +1,8 @@
-public class count-ways-to-nth-stair {
-    
+class Solution {
+    // Function to count number of ways to reach the nth stair
+    // when order does not matter.
+    Long countWays(int n) {
+        // your code here
+        return (long)n/2+1;
+    }
 }
