be77ec258c05169e226bb3b2ee7380d00c297c50
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-04-07 20:27:47 +0530

    7th apr updated

--- maximize-dot-product.java
+++ maximize-dot-product.java
@@ -1,3 +1,20 @@
-public class maximize-dot-product {
-    
+class Solution {
+    int help(int i,int j,int n,int m,int a[],int b[],int dp[][],int diff){
+	    if(i==n || j==m)return 0;
+	    if(dp[i][j]!=-1)return dp[i][j];
+	    int sum1=0,sum2=0;
+	    if(diff!=0)
+	    sum1 = help(i+1,j,n,m,a,b,dp,diff-1);
+	    sum2 =a[i]*b[j]+help(i+1,j+1,n,m,a,b,dp,diff);
+	    return dp[i][j] = Math.max(sum1,sum2);
+	}
+	public int maxDotProduct(int n, int m, int a[], int b[]) 
+	{ 
+		// Your code goes here
+		int dp[][] = new int[n][m];
+		for(int temp[]:dp){
+		    Arrays.fill(temp,-1);
+		}
+		return help(0,0,n,m,a,b,dp,n-m);
+	} 
 }
