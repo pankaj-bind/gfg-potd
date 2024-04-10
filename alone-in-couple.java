e1b9ec1eed9b9cee2bbf39753b5f7623eecc21f4
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-04-10 22:30:26 +0530

    10th apr updated

--- alone-in-couple.java
+++ alone-in-couple.java
@@ -1,3 +1,8 @@
-public class alone-in-couple {
-    
+class Solution{
+    static int findSingle(int n, int arr[]){
+        // code here
+        int x=0;
+        for(int i=0;i<n;i++)x^=arr[i];
+        return x;
+    }
 }
