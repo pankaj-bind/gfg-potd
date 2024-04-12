a5368450472fda899a600c01528e2d28f2d9554b
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-04-12 19:18:05 +0530

    12th apr updated

--- sum-of-products.java
+++ sum-of-products.java
@@ -1,3 +1,24 @@
-public class sum-of-products {
-    
+class Solution {
+    static long pairAndSum(int n, long arr[]) {
+        // code here
+        int bits[] = new int[32];
+        Arrays.fill(bits,0);
+        for(int i=0;i<n;i++){
+            long temp = arr[i];
+            int j=0;
+            while(temp!=0){
+                if((temp&1)==1)bits[j]++;
+                temp>>=1;
+                j++;
+            }
+        }
+        long ans = 0;
+        for(int i=0;i<32;i++){
+            long temp = bits[i];
+            temp--;
+            long pairs = (temp*(temp+1))/2;
+            ans+=(pairs*(long)Math.pow(2,i));
+        }
+        return ans;
+    }
 }
