0e01c2051b01795bcd9e30cfdac772a69eef44b3
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-04-14 03:06:08 +0530

    14th apr updated

--- xoring-and-clearing.java
+++ xoring-and-clearing.java
@@ -1,3 +1,20 @@
-public class xoring-and-clearing {
-    
+class Solution {
+    public void printArr(int n, int arr[]) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println(); // Print a newline after printing the array
+    }
+
+    public void setToZero(int n, int arr[]) {
+        for (int i = 0; i < n; i++) {
+            arr[i] = 0; // Set each element to zero
+        }
+    }
+
+    public void xor1ToN(int n, int arr[]) {
+        for (int i = 0; i < n; i++) {
+            arr[i] ^= i; // Bitwise XOR each element with its index
+        }
+    }
 }
