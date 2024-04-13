e6e2a18ba4321ccb57fce248a3907a5c448e33d6
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-04-13 20:52:27 +0530

    13th apr updated

--- reverse-bits.java
+++ reverse-bits.java
@@ -1,3 +1,17 @@
-public class reverse-bits {
-    
+class Solution {
+    static Long reversedBits(Long x) {
+        long result = 0;
+        int bitPosition = 31; // since we're dealing with 32-bit integers
+        while (x != 0) {
+            // Check if the rightmost bit of x is set
+            if ((x & 1) == 1) {
+                // If set, set the corresponding bit in result
+                result |= (1L << bitPosition);
+            }
+            // Right shift x to move to the next bit
+            x >>= 1;
+            bitPosition--; // Move to the next bit position in result
+        }
+        return result;
+    }
 }
