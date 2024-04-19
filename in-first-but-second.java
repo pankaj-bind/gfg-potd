b1d38d08298033a03e8d8aca491f9546e74148e5
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-04-19 15:04:27 +0530

    19th apr updated

--- in-first-but-second.java
+++ in-first-but-second.java
@@ -1,3 +1,23 @@
-public class in-first-but-second {
-    
+class Solution
+{
+    ArrayList<Integer> findMissing(int a[], int b[], int n, int m)
+    {
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        // Create a HashSet to store elements of array b
+        HashSet<Integer> setB = new HashSet<>();
+        for (int num : b) {
+            setB.add(num);
+        }
+        
+        // Iterate through array a
+        for (int num : a) {
+            // If the element is not present in setB, add it to the result
+            if (!setB.contains(num)) {
+                result.add(num);
+            }
+        }
+        
+        return result;
+    }
 }
