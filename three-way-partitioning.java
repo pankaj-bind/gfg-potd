e7f2edc88de15c5e8746305c4dfcee5066c81f55
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-04-21 19:12:04 +0530

    21st apr updated

--- three-way-partitioning.java
+++ three-way-partitioning.java
@@ -1,3 +1,29 @@
-public three-way-partitioning {
+class Solution{
+    //Function to partition the array around the range such 
+    //that array is divided into three parts.
+    public void threeWayPartition(int array[], int a, int b)
+    {
+        int n = array.length;
+        int low = 0, mid = 0, high = n - 1;
+        
+        while (mid <= high) {
+            if (array[mid] < a) {
+                swap(array, low, mid);
+                low++;
+                mid++;
+            } else if (array[mid] >= a && array[mid] <= b) {
+                mid++;
+            } else {
+                swap(array, mid, high);
+                high--;
+            }
+        }
+    }
     
+    //Function to swap two elements of an array.
+    private void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
