e31eb81d1cc80c9c4d738fdffe63fedc17b27e19
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-05-07 22:44:10 +0530

    7th may updated

--- reverse.java
+++ reverse.java
@@ -1,3 +1,32 @@
-public class reverse-level-order-traversal {
-    
+class Tree
+{
+    public ArrayList<Integer> reverseLevelOrder(Node node) 
+    {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (node == null)
+            return result;
+
+        Queue<Node> queue = new LinkedList<>();
+        Stack<Node> stack = new Stack<>();
+
+        queue.offer(node);
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            stack.push(current);
+
+            if (current.right != null)
+                queue.offer(current.right);
+
+            if (current.left != null)
+                queue.offer(current.left);
+        }
+
+        while (!stack.isEmpty()) {
+            Node current = stack.pop();
+            result.add(current.data);
+        }
+
+        return result;
+    }
 }
