b9700d27404258a2b152994561e330ff7cfa2cb9
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-05-18 22:39:50 +0530

    18th may updated

--- find-the-highest-number.java
+++ find-the-highest-number.java
@@ -1,3 +1,19 @@
-public class find-the-highest-number {
-    
+class Solution {
+    public int findPeakElement(List<Integer> a) {
+        int left = 0;
+        int right = a.size() - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (a.get(mid) > a.get(mid + 1)) {
+                // Peak is in the left half including mid
+                right = mid;
+            } else {
+                // Peak is in the right half excluding mid
+                left = mid + 1;
+            }
+        }
+        // left and right will converge to the peak element
+        return a.get(left);
+    }
 }
