b3605e598bb8d62b694c5144176285ff926959ba
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-05-23 19:05:19 +0530

    23rd may updated

--- find-if-string-is-k-palindrome-or-not.java
+++ find-if-string-is-k-palindrome-or-not.java
@@ -1,3 +1,22 @@
-public class find-if-string-is-k-palindrome-or-not {
-    
+class Solution {
+    public int kPalindrome(String str, int n, int k) {
+        String reverse = new StringBuilder(str).reverse().toString();
+        int[][] dp = new int[n + 1][n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                } else if (j == 0) {
+                    dp[i][j] = i;
+                } else if (str.charAt(i - 1) == reverse.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[n][n] <= 2 * k ? 1 : 0;
+    }
 }
