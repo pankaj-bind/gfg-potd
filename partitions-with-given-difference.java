63bd2a0970a41e5f72d124d50c08160ab8a671e3
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-05-24 18:48:12 +0530

    24th may updated

--- partitions-with-given-difference.java
+++ partitions-with-given-difference.java
@@ -1,3 +1,26 @@
-public class partitions-with-given-difference {
-    
+class Solution {
+    public static int countPartitions(int n, int d, int[] arr) {
+        int MOD = 1000000007;
+        int sumArr = 0;
+        for (int num : arr) {
+            sumArr += num;
+        }
+
+        if ((sumArr + d) % 2 != 0 || sumArr < d) {
+            return 0;
+        }
+
+        int target = (sumArr + d) / 2;
+
+        int[] dp = new int[target + 1];
+        dp[0] = 1; // There's one way to make sum 0, by choosing no elements
+
+        for (int num : arr) {
+            for (int j = target; j >= num; j--) {
+                dp[j] = (dp[j] + dp[j - num]) % MOD;
+            }
+        }
+
+        return dp[target];
+    }
 }
