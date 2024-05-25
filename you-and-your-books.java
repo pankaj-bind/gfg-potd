29994b4f06bed241d42f70ac9fa0b328c2eb47f9
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-05-25 21:09:12 +0530

    25th may updated

--- you-and-your-books.java
+++ you-and-your-books.java
@@ -1,3 +1,16 @@
-public class you-and-your-books {
-    
+class Solution {
+    long max_Books(int arr[], int n, int k) {
+        long max_books = 0;
+        long current_sum = 0;
+
+        for (int end = 0; end < n; end++) {
+            if (arr[end] <= k) {
+                current_sum += arr[end];
+                max_books = Math.max(max_books, current_sum);
+            } else {
+                current_sum = 0;
+            }
+        }
+        return max_books;
+    }
 }
