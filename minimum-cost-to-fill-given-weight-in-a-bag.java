fb4739e6e9115cf651a1087e9702e515083b7e51
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-05-28 21:20:09 +0530

    28th may updated

--- minimum-cost-to-fill-given-weight-in-a-bag.java
+++ minimum-cost-to-fill-given-weight-in-a-bag.java
@@ -1,3 +1,17 @@
-public class minimum-cost-to-fill-given-weight-in-a-bag {
-    
+
+class Solution {
+    public static int minimumCost(int n, int w, int[] cost) {
+        // code here
+        int dp[] = new int[w+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0]=0;
+        for(int i=0;i<n;i++){
+            if(cost[i]!=-1){
+                for(int j=i+1;j<=w;j++){
+                    dp[j]=Math.min(dp[j],cost[i]+dp[j-i-1]);
+                }
+            }
+        }
+        return dp[w]!=Integer.MAX_VALUE?dp[w]:-1;
+    }
 }
