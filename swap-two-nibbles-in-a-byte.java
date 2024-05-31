1721da873934022ca2b0f3ddc1226f779427bca0
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-05-31 19:43:44 +0530

    31st may updated

--- swap-two-nibbles-in-a-byte.java
+++ swap-two-nibbles-in-a-byte.java
@@ -1,3 +1,10 @@
-public class swap-two-nibbles-in-a-byte {
-    
+class Solution {
+    static int swapNibbles(int n) {
+        // Shift the lower nibble (rightmost 4 bits) to the left by 4 positions
+        int leftNibbleShifted = (n & 0x0F) << 4;
+        // Shift the higher nibble (leftmost 4 bits) to the right by 4 positions
+        int rightNibbleShifted = (n & 0xF0) >> 4;
+        // Combine the shifted nibbles using bitwise OR
+        return leftNibbleShifted | rightNibbleShifted;
+    }
 }
