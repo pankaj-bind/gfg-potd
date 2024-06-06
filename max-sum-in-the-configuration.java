df168d8cdfe70e8792c39190fcbc059846f26987
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-06-06 21:21:35 +0530

    6th june updated

--- max-sum-in-the-configuration.java
+++ max-sum-in-the-configuration.java
@@ -1,3 +1,20 @@
-public class max-sum-in-the-configuration {
-    
+class Solution {
+    long max_sum(int arr[], int n) {
+        // Your code here
+        long sum=0;
+        long prev_sum=0;
+        for(int i=0; i<n; i++){
+            prev_sum +=  (long)i*arr[i];
+            sum+=arr[i];
+        }
+        long ans=prev_sum;
+        
+        for(int i=1; i<n; i++){
+            long curr_sum = prev_sum - (sum -arr[i-1]) + (long)arr[i-1]*(n-1);
+            
+            prev_sum = curr_sum;
+          if(curr_sum > ans) ans = curr_sum;
+        }
+        return ans;
+    }
 }
