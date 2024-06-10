1a7b03e368f826c1f9a1e93d88ac355da05cbe93
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-06-10 22:47:26 +0530

    10th june updated

--- nuts-and-bolts-problem.java
+++ nuts-and-bolts-problem.java
@@ -1,3 +1,16 @@
-public class nuts-and-bolts-problem {
-    
+class Solution {
+    void matchPairs(int n, char nuts[], char bolts[]) {
+        // code here
+        char temp[]={'!','#','$','%','&','*','?','@','^'};
+        Map<Character,Integer> mm = new HashMap<>();
+        for(int i=0;i<n;i++)mm.put(nuts[i],1);
+        int j=0;
+        for(int i=0;i<9;i++){
+            if(mm.get(temp[i])!=null){
+                nuts[j]=temp[i];
+                bolts[j]=temp[i];
+                j++;
+            }
+        }
+    }
 }
