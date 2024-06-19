9d8f202e5a6a184637992159cbd8b209d0656846
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-06-19 21:58:14 +0530

    19th june updated

--- find-maximum-volume-of-a-cuboid.java
+++ find-maximum-volume-of-a-cuboid.java
@@ -1,3 +1,14 @@
-public class magical-box {
-    
+class Solution {
+
+    double maxVolume(double P, double A) {
+        // code here
+         // calculate length
+        double l
+            = (double)(P - Math.sqrt(P * P - 24 * A)) / 12;
+ 
+    double height = (P / 4) - (2 * (l));
+        
+         double ans = l * l* height;
+          return ans;
+    }
 }
