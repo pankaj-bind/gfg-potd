7a69f399a994245e11ffec9c72f0c6e3dbbb4bf1
Pankaj Kumar Bind <73558583+pankaj-bind@users.noreply.github.com>
2024-09-07 20:41:13 +0530

    Update nth-natural-number.java

--- nth-natural-number.java
+++ nth-natural-number.java
@@ -1,1 +1,17 @@
 
+class Solution {
+    long findNth(long n) {
+        long result = 0;
+        long base = 1;
+        
+        while (n > 0) {
+           
+            result += (n % 9) * base;
+  
+            n /= 9;
+            base *= 10;
+        }
+        
+        return result;
+    }
+}
