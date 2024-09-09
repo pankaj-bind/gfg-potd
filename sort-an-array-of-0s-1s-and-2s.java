e880ae8a9c4cda62d4888bdac82275e2551cc572
Pankaj Kumar Bind <73558583+pankaj-bind@users.noreply.github.com>
2024-09-09 20:51:30 +0530

    Update sort-an-array-of-0s-1s-and-2s.java

--- sort-an-array-of-0s-1s-and-2s.java
+++ sort-an-array-of-0s-1s-and-2s.java
@@ -1,1 +1,28 @@
+class Solution {
+    // Function to sort an array of 0s, 1s, and 2s
+    public void sort012(ArrayList<Integer> arr) {
+        int low = 0, mid = 0, high = arr.size() - 1;
 
+        while (mid <= high) {
+            switch (arr.get(mid)) {
+                case 0:
+                    // Swap arr[low] and arr[mid]
+                    Collections.swap(arr, low, mid);
+                    low++;
+                    mid++;
+                    break;
+
+                case 1:
+                    // No swap needed, just move mid pointer
+                    mid++;
+                    break;
+
+                case 2:
+                    // Swap arr[mid] and arr[high]
+                    Collections.swap(arr, mid, high);
+                    high--;
+                    break;
+            }
+        }
+    }
+}
