111aedcdaf1552b691c1056b6409921858bc560b
Pankaj Kumar Bind <73558583+pankaj-bind@users.noreply.github.com>
2024-09-11 12:50:06 +0530

    Update minimum-cost-of-ropes.java

--- minimum-cost-of-ropes.java
+++ minimum-cost-of-ropes.java
@@ -1,1 +1,23 @@
-
+class Solution {
+    public long minCost(long[] arr) {
+        PriorityQueue<Long> minHeap = new PriorityQueue<>();
+        
+        for (long length : arr) {
+            minHeap.add(length);
+        }
+        
+        long totalCost = 0;
+        
+        while (minHeap.size() > 1) {
+            long first = minHeap.poll();
+            long second = minHeap.poll();
+            
+            long cost = first + second;
+            totalCost += cost;
+            
+            minHeap.add(cost);
+        }
+        
+        return totalCost;
+    }
+}
