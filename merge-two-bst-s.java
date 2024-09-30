9dd32c69167bd6bf8513a72a0a80274596362dcd
Pankaj Kumar Bind <73558583+pankaj-bind@users.noreply.github.com>
2024-09-30 18:10:38 +0530

    Create merge-two-bst-s.java

---
+++ merge-two-bst-s.java
@@ -0,0 +1,45 @@
+class Solution {
+    private void inorder(Node root, List<Integer> result) {
+        if (root == null) return;
+        inorder(root.left, result);
+        result.add(root.data);
+        inorder(root.right, result);
+    }
+
+    private List<Integer> mergeSortedLists(List<Integer> list1, List<Integer> list2) {
+        List<Integer> mergedList = new ArrayList<>();
+        int i = 0, j = 0;
+
+        while (i < list1.size() && j < list2.size()) {
+            if (list1.get(i) <= list2.get(j)) {
+                mergedList.add(list1.get(i));
+                i++;
+            } else {
+                mergedList.add(list2.get(j));
+                j++;
+            }
+        }
+
+        while (i < list1.size()) {
+            mergedList.add(list1.get(i));
+            i++;
+        }
+
+        while (j < list2.size()) {
+            mergedList.add(list2.get(j));
+            j++;
+        }
+
+        return mergedList;
+    }
+
+    public List<Integer> merge(Node root1, Node root2) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        inorder(root1, list1);
+        inorder(root2, list2);
+
+        return mergeSortedLists(list1, list2);
+    }
+}
