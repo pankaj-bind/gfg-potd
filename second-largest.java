89633d62eff57a7a88dbce6c0b7f4a2dbeb8563d
Pankaj Kumar Bind <73558583+pankaj-bind@users.noreply.github.com>
2024-11-15 22:14:32 +0530

    Create second-largest.java

---
+++ second-largest.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num < largest) {
+                secondLargest = num;
+            }
+        }
+        
+        return secondLargest == Integer.MIN_VALUE ? -1 : secondLargest;
+    }
+}
