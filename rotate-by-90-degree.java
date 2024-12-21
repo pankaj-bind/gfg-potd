3b696c8228846c7efee06fb20821ae198da26a59
Pankaj Kumar Bind <73558583+pankaj-bind@users.noreply.github.com>
2024-12-21 09:22:04 +0530

    Create rotate-by-90-degree.java

---
+++ rotate-by-90-degree.java
@@ -0,0 +1,28 @@
+
+class Solution {
+    // Function to rotate matrix anticlockwise by 90 degrees.
+    static void rotateby90(int mat[][]) {
+        int n = mat.length;
+
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each column
+        for (int j = 0; j < n; j++) {
+            int top = 0, bottom = n - 1;
+            while (top < bottom) {
+                int temp = mat[top][j];
+                mat[top][j] = mat[bottom][j];
+                mat[bottom][j] = temp;
+                top++;
+                bottom--;
+            }
+        }
+    }
+}
